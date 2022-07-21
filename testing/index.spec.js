367dd7e9425b1391b25fcead7d16505f724fca20
Florencia Pezcara <fpezcara@gmail.com>
2022-07-21 17:39:28 +0100

    add 2 more tests

--- testing/index.spec.js
+++ testing/index.spec.js
@@ -1,16 +1,26 @@
-const renderDom = require('./helpers')
+const renderDom = require("./helpers");
 
 let dom;
 let document;
 
-describe('index.html', () => {
-    beforeEach(async () => {
-        dom = await renderDom('index.html');
-        document = await dom.window.document;
-    })
+describe("index.html", () => {
+  beforeEach(async () => {
+    dom = await renderDom("index.html");
+    document = await dom.window.document;
+  });
 
-    test('it has a header title', () => {
-        let nav = document.querySelector('nav');
-        expect(nav).toBeTruthy();
-    })
-})
+  test("it has a nav", () => {
+    let nav = document.querySelector("nav");
+    expect(nav).toBeTruthy();
+  });
+
+  test("it has a main section", () => {
+    let main = document.querySelector("main");
+    expect(main).toBeTruthy();
+  });
+
+    test("it has 2 anchor tags in nav section at start", () => {
+      let a = document.querySelectorAll(".links a")
+      expect(a.length).toBe(2);
+    });
+});
